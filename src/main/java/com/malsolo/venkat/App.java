e5f5e710297e09415e9abe86b6b2c0e59f0b94da
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-11-10 16:46:17 +0100

    java-venkat: java 8 and junit 4.11

--- src/main/java/com/malsolo/venkat/App.java
+++ src/main/java/com/malsolo/venkat/App.java
@@ -1,13 +1,21 @@
 package com.malsolo.venkat;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        Arrays.asList(1, 2, 3, 4, 5)
+            .stream()
+            .filter(n -> n % 2 == 0)
+            .map(n -> n*2)
+            .forEach(System.out::println);
+        ;
+
     }
 }
